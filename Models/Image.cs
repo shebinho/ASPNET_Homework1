d12868424c0a45962cab6009de213d9527d694bb
shebinho <33525543+shebinho@users.noreply.github.com>
2018-05-17 04:37:02 +0200

    Add files via upload

---
+++ Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoApp.WebMvc.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string Description { get; set; }
+        public string DisplayName { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
